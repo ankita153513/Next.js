90069f6f3305d0aedd951906edf829e53b5269fc
ankita <ankita2911032gmail.com>
2026-05-03 15:49:04 +0530

    fix issue

---
+++ app/helpers/mailer.ts
@@ -0,0 +1,53 @@
+import nodemailer from 'nodemailer';
+import User  from "@/app/models/userModel";
+import bcryptjs from'bcryptjs';
+
+export const sendEmail = async({email, emailType, userId: any}) => {
+
+try{
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+
+    if(emailType === "VERIFY") {
+
+    await User.findByIdAndUpdate(userId, 
+        {verifyToken: hashedToken, 
+            verifyTokenExpiry:  Date.now() + 3600000})
+        } else if (emailType === "RESET") {
+            await User.findByIdAndUpdate(userId, 
+        {verifyToken: hashedToken, 
+            verifyTokenExpiry:  Date.now() + 3600000})
+
+        }
+
+        var transport = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "2b11f9edb0df16",
+    pass: "****2a44"
+  }
+});
+
+const mailOptions = {
+    from: 'ankita291103@gmail.com',
+    to: email,
+    subject: emailType === "VERIFY" ? "Verify your email" 
+    : "Reset your password",
+    html: `<p>Click <a href="${process.env.DOMAIN}/
+    verifyemail?token=${hashedToken}">here</a>
+    to $ {emailType === "VERIFY" ? "verify your email" : 
+    "reset your passwor"}
+    or copy and paste the link below in your browser.
+    <br> $ {process.env.DOMAIN}/verifyemail?
+    token=${hashedToken}
+    </p>`
+}
+
+const mailresponse = await transport.sendMail(mailOptions);
+return mailresponse;
+
+
+} catch (error: any) {
+    throw new Error(error.message);
+}
+}
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,38 @@
 "use client";
 import axios from "axios";
-import Link from "next/link";    
+import Link from "next/link";  
+import React, { useState } from "react";  
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { set } from "mongoose";
+
+
 export default function UserProfilePage() {
+  const router = useRouter()
+const logout = async  () => {
+
+
+    try{
+      await axios.get('/api/users/logout')
+      toast.success('Logout successful')
+      router.push('/login')
+
+
+    } catch  (error: any) {
+      console.log(error.message);
+      toast.error(error.message)
+
+    }
+  }
+
+
+const[data, setData ] = useState("nothing")
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me')
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center bg-black text-white">
       
@@ -12,10 +43,21 @@ export default function UserProfilePage() {
         Profile page
 
       </p>
+      <h2 className="p-1 rounded bg-green-500">{data === 'nothing'? "Nothing" : <Link 
+      href={'/profile/${data}'}>{data}
+      </Link>}</h2>
       <hr/>
       <button
       onClick={logout}
-        className="bg-blue-500  mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
+        className="bg-blue-500  mt-4 hover:bg-blue-700
+         text-white font-bold py-2 px-4 rounded"
+         >Logout</button>
+
+         <button
+      onClick={getUserDetails}
+        className="bg-green-800  mt-4 hover:bg-blue-700
+         text-white font-bold py-2 px-4 rounded"
+         >GetUserDetails</button>
 
     </div>
   );
